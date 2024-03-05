9c9716e5c5582de1e63ff43118414fc7d947daf9
James Bartolotti <48363553+jbartolotti@users.noreply.github.com>
2024-03-05 13:49:02 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'HBIC Computing'
+copyright = '2024, James Bartolotti'
+author = 'James Bartolotti'
 
 release = '0.1'
 version = '0.1.0'
